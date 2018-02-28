ed22c3045cff6bd00b017de838b1219889ae2ea9
Joseph Moussa <jmoussa@Josephs-MacBook-Pro.local>
2018-02-28 17:48:11 -0500

    Don't rush me ><

--- client/src/js/pages/Output.js
+++ client/src/js/pages/Output.js
@@ -5,7 +5,7 @@ class Output extends Component {
   render() {
     return (
       <div className="Output">
-       <p>Here is the output</p>
+       <p>DON'T RUSH ME! I'm still working on adding input to the database. This feature will be available soon...</p>
       </div>
     );
   }
