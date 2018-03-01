e02ff02f738526400e0065264d985e65f0c7ee02
Joseph Moussa <jmoussa@Josephs-MacBook-Pro.local>
2018-03-01 00:03:46 -0500

    got rid of view layer in the server (not needed since we use ReactJS as the view)

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ var connection = mysql.createConnection({
 });
 
 connection.connect(function(err){
-    if(err) throw err
+    if(err) throw err;
     console.log("You are now connected to the database");
 });
 
@@ -21,37 +21,30 @@ connection.connect(function(err){
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-//Set default view engine
+//Enable CORS
+const cors = require('cors');
+app.use(cors());
+app.options('*', cors());
+
+//Set default view engine 
+//Not needed since we're using react for the view layer
+/*
 var handlebars = require('express-handlebars').create({defaultLayout:'main'});
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 app.set('port', port);
 app.use(express.static(__dirname + '/public'));
 
-//Enable CORS
-const cors = require('cors');
-app.use(cors());
-app.options('*', cors());
-
-/*
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header("Access-Control-Allow-Methods", "OPTIONS, GET, HEAD, POST, PUT");
-  res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
-  next();
-});
-*/
-
 app.get('/', function(req,res){
     return res.send({error: false, message:'hello'})
 });
+*/
 
 app.post('/', function(req,res){
   var queryString = "INSERT INTO MERCHANT(storeName, storeAddress, storePhone) VALUES ('" + req.body.storeName +  "', '" + req.body.storeAddress + "', '" + req.body.storePhone + "');";
   console.log(queryString + "\n");
   connection.query(queryString, function(err, result){
-      if(err) throw err
+      if(err) throw err;
       console.log("Query Successful...");
       
       res.send('{"status": "merchant success"}');
