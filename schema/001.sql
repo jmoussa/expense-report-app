049db391b43a17755ce3f4b8b643f0cfafeff587
Joseph Moussa <jmoussa@Josephs-MacBook-Pro.local>
2018-03-12 13:53:30 -0400

    Added PRODUCT Table (to track products purchased) to database and form control

--- schema/001.sql
+++ schema/001.sql
@@ -32,3 +32,15 @@ CREATE TABLE `TRANSACTIONS` (
   CONSTRAINT `transactions_ibfk_1` FOREIGN KEY (`categoryID`) REFERENCES `CATEGORIES` (`cID`),
   CONSTRAINT `transactions_ibfk_2` FOREIGN KEY (`storeID`) REFERENCES `MERCHANT` (`mID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `PRODUCTS` (
+  `pID` int(11) NOT NULL AUTO_INCREMENT,
+  `productName` varchar(120) DEFAULT NULL,
+  `transaction_ID` int(11) DEFAULT NULL,
+  `merchant_ID` int(11) DEFAULT NULL,
+  PRIMARY KEY (`pID`),
+  KEY `transaction_ID` (`transaction_ID`),
+  KEY `merchant_ID` (`merchant_ID`),
+  CONSTRAINT `products_ibfk_1` FOREIGN KEY (`transaction_ID`) REFERENCES `TRANSACTIONS` (`tID`),
+  CONSTRAINT `products_ibfk_2` FOREIGN KEY (`merchant_ID`) REFERENCES `MERCHANT` (`mID`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
